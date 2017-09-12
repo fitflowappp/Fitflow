0e6259fc04f45d3fc07d597bcfb770af6a51dd4d
ljy <18513813964@163.comm>
2017-09-12 10:01:38 +0800

    Initial Commit

---
+++ Yoga/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Yoga
+//
+//  Created by lyj on 2017/9/12.
+//  Copyright © 2017年 lyj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Yoga/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Yoga
+//
+//  Created by lyj on 2017/9/12.
+//  Copyright © 2017年 lyj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
