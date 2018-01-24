a3a0132f96f287a42846fb9fb00b2775fe346216
ljy <18513813964@163.comm>
2018-01-24 17:14:42 +0800

    alert font change

--- Yoga/core/hud/YGTopAlert.m
+++ Yoga/core/hud/YGTopAlert.m
@@ -24,30 +24,24 @@
 
 -(void)addAlertImgv{
     self.alertImgv = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"error-top-logo"]];
-    self.alertImgv.center = CGPointMake(16*SCALE+self.alertImgv.frame.size.width/2,29*SCALE+20);
+    self.alertImgv.center = CGPointMake(16+self.alertImgv.frame.size.width/2,29+20);
     [self addSubview:self.alertImgv];
-    
 }
 
 -(void)addAlertLabel{
-    CGFloat marginX = CGRectGetMaxX(self.alertImgv.frame)+16*SCALE;
-    self.alertLabel = [[UILabel alloc] initWithFrame:CGRectMake(marginX,20,self.frame.size.width-marginX-16*SCALE,self.frame.size.height-20)];
-    self.alertLabel.textColor = [UIColor whiteColor];
+    CGFloat marginX = CGRectGetMaxX(self.alertImgv.frame)+16;
+    self.alertLabel = [[UILabel alloc] initWithFrame:CGRectMake(marginX,20,self.frame.size.width-marginX-16,self.frame.size.height-20)];
     self.alertLabel.numberOfLines = 0;
+    self.alertLabel.textColor = [UIColor whiteColor];
     self.alertLabel.backgroundColor = [UIColor clearColor];
-    self.alertLabel.font = [UIFont fontWithName:@"Lato-Regular" size:14*SCALE];
+    self.alertLabel.font = [UIFont fontWithName:@"Lato-Regular" size:14];
     [self addSubview:self.alertLabel];
 }
 
-
-
-
 +(YGTopAlert*)alert:(NSString*)msg bkColorCode:(NSString*)code{
-    
-    YGTopAlert *alert = [[YGTopAlert alloc] initWithFrame:CGRectMake(0,-58*SCALE-20,GET_SCREEN_WIDTH,58*SCALE+20)];
+    YGTopAlert *alert = [[YGTopAlert alloc] initWithFrame:CGRectMake(0,-58-20,GET_SCREEN_WIDTH,58+20)];
     alert.backgroundColor = [UIColor colorWithHexString:code];
     alert.alertLabel.text = msg;
-    
     UIWindow *mainWindow = [UIApplication sharedApplication].delegate.window;
     for (UIView *v in mainWindow.subviews) {
         if ([v isKindOfClass:[YGTopAlert class]]) {
@@ -56,12 +50,12 @@
     }
     [mainWindow addSubview:alert];
     [alert show];
-   return alert;
+    return alert;
 }
 
 -(void)show{
     [UIView animateWithDuration:0.5 delay:0 usingSpringWithDamping:1 initialSpringVelocity:20 options:UIViewAnimationOptionLayoutSubviews animations:^{
-        self.frame = CGRectMake(0,0,GET_SCREEN_WIDTH,58*SCALE+20);
+        self.frame = CGRectMake(0,0,GET_SCREEN_WIDTH,58+20);
     } completion:^(BOOL finished) {
         [self hide];
     }];
@@ -69,7 +63,7 @@
 
 -(void)hide{
     [UIView animateWithDuration:0.5 delay:3 usingSpringWithDamping:1 initialSpringVelocity:20 options:UIViewAnimationOptionLayoutSubviews animations:^{
-        self.frame = CGRectMake(0,-58*SCALE-20,GET_SCREEN_WIDTH,58*SCALE+20);
+        self.frame = CGRectMake(0,-58-20,GET_SCREEN_WIDTH,58+20);
     } completion:^(BOOL finished) {
         [self removeFromSuperview];
     }];
